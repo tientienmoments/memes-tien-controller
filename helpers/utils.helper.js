5bbab6fc662a22d5a1dcb8cded82c8bd6979741e
tientienmoments <tientienmoments@gmail.com>
2020-08-14 18:51:56 +0700

    first

--- helpers/utils.helper.js
+++ helpers/utils.helper.js
@@ -1,6 +1,6 @@
 "use strict";
 const utilsHelper = {};
-
+const crypto = require("crypto");
 // This function controls the way we response to the client
 // If we need to change the way to response later on, we only need to handle it here
 utilsHelper.sendResponse = (res, status, success, data, error, message) => {
@@ -10,6 +10,13 @@ utilsHelper.sendResponse = (res, status, success, data, error, message) => {
   if (error) response.error = { message: error.message };
   if (message) response.message = message;
   return res.status(status).json(response);
+};
+utilsHelper.generateRandomHexString = (len) => {
+  return crypto
+    .randomBytes(Math.ceil(len / 2))
+    .toString("hex") // convert to hexadecimal format
+    .slice(0, len)
+    .toUpperCase(); // return required number of characters
 };
 
 module.exports = utilsHelper;
--- routes/memeApi.js
+++ routes/memeApi.js
@@ -1,17 +1,37 @@
 var express = require("express");
 var router = express.Router();
+const photoHelper = require("../helpers/photo.helper");
 
+const fileUpload = require("../helpers/upload.helper")("public/images/");
+const uploader = fileUpload.uploader;
+const memeController = require("../controllers/memeController")
 /**
  * @route GET api/memes
  * @description Get all memes
  * @access Public
  */
-router.get("/", function (req, res, next) {
-  res.send({ status: "ok", data: "Get all memes" });
-});
+router.get("/", memeController.getMemes);
 
 router.get("/abc", function (req, res, next) {
   res.send({ status: "ok", data: "ABC" });
 });
+/**
+* @route POST api/memes
+* @description Create a new meme
+* @access Public
+*/
+router.post(
+  "/",
+  uploader.single("image"),
+  photoHelper.resize,
+  memeController.createMeme
+);
+/**
+ * @route GET api/memes/images
+ * @description Get all memes
+ * @access Public
+ */
+router.get("/images", memeController.getOriginalImages);
+router.put('/:id', memeController.updateMeme);
 
 module.exports = router;
